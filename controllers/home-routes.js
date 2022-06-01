d1b3351672cb701ce0f015aee9f566711d07c565
Leandro Michelena <leandromichelena@gmail.com>
2022-06-01 00:15:19 -0400

    Created home-routes and installed nodemon package

---
+++ controllers/home-routes.js
@@ -0,0 +1,27 @@
+const router = require('express').Router();
+const { Plants, User, Comment } = require('../models');
+
+router.get('/homepage', async (req, res) => {
+  res.render('homepage');
+});
+
+router.get('/dashboard', async (req, res) => {
+  // show dashboard if logged in, otherwise, redirect to login page
+  if (req.session.loggedIn) {
+    res.render('dashboard');
+    return;
+  }
+  res.redirect('/login');
+});
+
+router.get('/login', (req, res) => {
+  // If already logged in, redirect to the homepage
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
+
+  res.render('login');
+});
+
+module.exports = router;
