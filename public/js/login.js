dceca7f9a535f6d764200ebac4065d799550b254
maggiejoe <maggiejoseph94@gmail.com>
2022-06-03 15:37:31 -0400

    new attempt a linking login.js to login.handlebars

--- public/js/login.js
+++ public/js/login.js
@@ -7,17 +7,49 @@ function signupFormHandler(event) {
 
     if (username && email && password) {
         fetch('api/users', {
-        method: 'post',
-        body: JSON.stringify({
-            username,
-            email,
-            password
-        }),
-        headers: { 'Content-Type': 'application/json' }
-        }).then((response) => {console.log(response)})
+            method: 'post',
+            body: JSON.stringify({
+                username,
+                email,
+                password
+            }),
+            headers: { 'Content-Type': 'application/json' }
+        });
+
+        if (response.ok) {
+            document.location.replace('/dashboard/');
+        } else {
+            alert(response.statusText);
+        }
+    }
+}
+
+async function loginFormHandler(event) {
+    event.preventDefault();
+
+    const email = document.querySelector('#email-login').value.trim();
+    const password = document.querySelector('#password-login').value.trim();
+
+    if (email && password) {
+        const response = await fetch('/api/users/login', {
+            method: 'post',
+            body: JSON.stringify({
+                email,
+                password
+            }),
+            headers: { 'Content-Type': 'application/json' }
+        });
+
+        if (response.ok) {
+            document.location.replace('/dashboard/');
+        } else {
+            alert(response.statusText);
+        }
     }
 }
 
 
 
 document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
+
+document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
