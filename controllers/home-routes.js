6637b28c70d4af49947a355f11ea6074f125a8e6
Leandro Michelena <leandromichelena@gmail.com>
2022-06-05 22:03:07 -0400

    Updated home routes to display plants results

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,18 +1,19 @@
 const router = require('express').Router();
 // const sequelize = require('../config/connection');
 const { Plants, User, Comment } = require('../models');
+const plantData = require('../public/js/search-results');
 
 router.get('/', async (req, res) => {
   res.render('homepage', { loggedIn: req.session.loggedIn });
 });
 
 router.get('/dashboard', async (req, res) => {
-  // show dashboard if logged in, otherwise, redirect to login page
-  // if (req.session.loggedIn) {
+  // show dashboard if logged in, oth /erwise, redirect to login page
+  if (req.session.loggedIn) {
     res.render('dashboard',{ loggedIn:req.session.loggedIn });
-  //   return;
-  // }
-  // res.redirect('/login');
+    return;
+  }
+  res.redirect('/login');
 });
 
 router.get('/login', (req, res) => {
@@ -21,7 +22,6 @@ router.get('/login', (req, res) => {
     res.redirect('/');
     return;
   }
-
   res.render('login');
 });
 
@@ -31,20 +31,8 @@ router.get('/sign-up', (req, res) => {
 
 router.get('/results', async (req, res) => {
   // need to link js file to this on how to display queried data
-  res.render('results', { loggedIn: req.session.loggedIn });
+  const results = plantData;
+  res.render('results', { loggedIn: req.session.loggedIn , results: results });
 });
-
-// router.post('/logout', (req, res) => {
-//   if (req.session.loggedIn) {
-//     req.session.destroy(() => {
-//       res
-//         .status(204)
-//         .json({ message: 'You are now logged out!' })
-//         .end();
-//     });
-//   } else {
-//     res.status(400).end();
-//   }
-// });
 
 module.exports = router;
