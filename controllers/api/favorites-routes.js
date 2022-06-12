b68efdf1aee04fd99b2fad4cda85422c82efbc01
maggiejoe <maggiejoseph94@gmail.com>
2022-06-12 14:31:59 -0400

    commented out all favorite additions as they are currently not working

--- controllers/api/favorites-routes.js
+++ controllers/api/favorites-routes.js
@@ -1,71 +1,71 @@
-const router = require('express').Router();
-var sequelize = require('sequelize');
-const { Plants, User, Favorites } = require('../../models');
+// const router = require('express').Router();
+// var sequelize = require('sequelize');
+// const { Plants, User, Favorites } = require('../../models');
 
-router.get('/:userid', (req, res) => {
+// router.get('/:userid', (req, res) => {
 
-    // user the user id to get all the rows that have the same userid and then user the results plant ids to get alll the plants with those ids
-    Plants.findAll({
-        attributes: ['plant_id', 'plant_name'],
-        include: [
-            {
-                model: User,
-                through: Favorites,
-                as: 'users'
-            }
-        ]
-    })
-        .then(plantData => { res.json(plantData) })
-        .catch(err => {
-            console.log(err)
-            res.status(500).json(err)
-        })
-});
+//     // user the user id to get all the rows that have the same userid and then user the results plant ids to get alll the plants with those ids
+//     Plants.findAll({
+//         attributes: ['plant_id', 'plant_name'],
+//         include: [
+//             {
+//                 model: User,
+//                 through: Favorites,
+//                 as: 'users'
+//             }
+//         ]
+//     })
+//         .then(plantData => { res.json(plantData) })
+//         .catch(err => {
+//             console.log(err)
+//             res.status(500).json(err)
+//         })
+// });
 
-router.get('/users', (req, res) => {
-    User.findAll({
-        attributes: ['user_id', 'email'],
-        include: [
-            {
-                model: Plants,
-                through: Favorites,
-                as: 'favorite_plants'
-            }
-        ]
-    })
-        .then(userData => { res.json(userData) })
-        .catch(err => {
-            console.log(err)
-            res.status(500).json(err)
-        })
-});
+// router.get('/users', (req, res) => {
+//     User.findAll({
+//         attributes: ['user_id', 'email'],
+//         include: [
+//             {
+//                 model: Plants,
+//                 through: Favorites,
+//                 as: 'favorite_plants'
+//             }
+//         ]
+//     })
+//         .then(userData => { res.json(userData) })
+//         .catch(err => {
+//             console.log(err)
+//             res.status(500).json(err)
+//         })
+// });
 
-router.get('/user/:id', (req, res) => {
-    User.findOne({
-        where: {
-            user_id: req.params.id
-        },
-        attributes: ['user_id', 'email'],
-        include: [
-            {
-                model: Plants,
-                through: Favorites,
-                as: 'favorite_plants'
-            }
-        ]
-    })
-        .then(userData => {
-            if (!userData) {
-                res.status(404).json({ message: 'Could not find an user with this id' })
-                return
-            }
-            res.json(userData)
-        })
-        .catch(err => {
-            console.log(err)
-            res.status(500).json(err)
-        })
-});
+// router.get('/user/:id', (req, res) => {
+//     User.findOne({
+//         where: {
+//             user_id: req.params.id
+//         },
+//         attributes: ['user_id', 'email'],
+//         include: [
+//             {
+//                 model: Plants,
+//                 through: Favorites,
+//                 as: 'favorite_plants'
+//             }
+//         ]
+//     })
+//         .then(userData => {
+//             if (!userData) {
+//                 res.status(404).json({ message: 'Could not find an user with this id' })
+//                 return
+//             }
+//             res.json(userData)
+//         })
+//         .catch(err => {
+//             console.log(err)
+//             res.status(500).json(err)
+//         })
+// });
 
 // // this route will add the favorite plant ID to the list of favorites for a given user ID
 // router.put('/:userId/:plantId', (req, res) => {
@@ -107,31 +107,31 @@ router.get('/user/:id', (req, res) => {
 
 
 
-router.post('/', (req, res) => {
-    const {userid,plantid}=req.body
-    Favorites.create(
-        {
-            // Can't get this to work. We need to 'append' the plant ID number to the 'array' of favorite_plants
-           user_id:userid,
-            plant_id:plantid
-        },
+// router.post('/', (req, res) => {
+//     const {userid,plantid}=req.body
+//     Favorites.create(
+//         {
+//             // Can't get this to work. We need to 'append' the plant ID number to the 'array' of favorite_plants
+//            user_id:userid,
+//             plant_id:plantid
+//         },
    
-    )
-        .then(userData => {
-            if (!userData) {
-                res.status(404).json({ message: 'Could not find matching user.' })
-                return;
-            }
-            res.json(userData)
-        })
-        .catch(err => {
-            console.log(err)
-            res.status(500).json(err)
-        })
-});
+//     )
+//         .then(userData => {
+//             if (!userData) {
+//                 res.status(404).json({ message: 'Could not find matching user.' })
+//                 return;
+//             }
+//             res.json(userData)
+//         })
+//         .catch(err => {
+//             console.log(err)
+//             res.status(500).json(err)
+//         })
+// });
 
 
 
 // Add another user favorites get route
 
-module.exports = router;
+// module.exports = router;
